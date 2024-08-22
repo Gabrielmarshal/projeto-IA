cfb10fd6054499033d2b1ae54140e8e47b9b131f
Gabrielmarshal <127216625+Gabrielmarshal@users.noreply.github.com>
2024-08-22 10:43:48 -0300

    Add files via upload

---
+++ Inteligencia-artificial-3S-rie-main/inteligencia artificial/script.js
@@ -0,0 +1,52 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Como um torcedor pode afetar uma estação de trem inteira por causa de uma aposta errada?",
+
+        alternativas: ["Pode quebrar uma coisa ou outra da estação, mas logo pode ser contido e preso pela polícia.",
+            "Alternativa 2"
+        ]
+    },
+
+    {
+        enunciado: "Que medidas de segurança devem ser tomadas contra brigas de torcedores em relação a população que utiliza os transportes públicos?",
+        alternativas: ["No ingresso deve ser cobrado uma taxa adicional referente a um frete de ônibus, assim após o final da partida os torcedores serão direcionados aos seus respectivos transportes, evitando conflitos.  
+",
+ "Medida para combater a violência é, que um agente de segurança acompanhem os torcedores nos transportes públicos, privados para que a deslocação seja feita de forma pacífica."
+
+        },]
+{
+    enunciado: "Como um torcedor pode afetar uma estação de trem inteira por causa de uma aposta errada?",
+        alternativas: ["Influência de Grupos de Aposta",
+            "Aumento da Tensão em Áreas de Jogo "
+        ]
+},
+{
+    enunciado: "Pergunta2",
+        alternativas: ["Alternativa 1",
+            "Alternativa 2"
+        ]
+},
+{
+    enunciado: "Pergunta2",
+        alternativas: ["Alternativa 1",
+            "Alternativa 2"
+        ]
+}, 
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+
+}
+
+mostraPergunta();
